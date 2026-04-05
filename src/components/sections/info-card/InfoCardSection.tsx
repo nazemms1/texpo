84cb5f24c5c6d88a86c0eb9436ceb2f10c2fd2ee
Nazem Almsouti <90716419+nazemms1@users.noreply.github.com>
2026-04-05 10:07:51 +0300

    feat(texpo-2025):adding this page and style

--- src/components/sections/info-card/InfoCardSection.tsx
+++ src/components/sections/info-card/InfoCardSection.tsx
@@ -5,7 +5,7 @@ import styles from "./InfoCardSection.module.css";
 export function InfoCardSection() {
   return (
     <section className={styles.section}>
-      <Container size="full">
+      <Container size="full" >
         <InfoCard
           title="Global Expo"
           description="TEXPO LAND is considered a launchpad that brings together investors, innovators, and technology industry leaders from around the world, with the goal of driving digital transformation in Syria and the region at large, and achieving sustainable growth for businesses and investors. We are committed to facilitating business growth by promoting innovative technological solutions, strengthening strategic partnerships, and enabling cross-border collaboration. Through our events, we aim to accelerate the adoption of modern technologies and open new opportunities and markets for both investors and companies alike.."
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,10 +3,10 @@ import type { NavItem, StatItem } from "@/src/types";
 export const NAV_ITEMS: NavItem[] = [
   { key: "home", path: "" },
   { key: "about", path: "/about" },
-  // { key: "contact", path: "/contact" },
-  // { key: "sponsor", path: "/become-a-sponsor" },
-  // { key: "visitors", path: "/visitors" },
-  // { key: "texpo2025", path: "/texpo-2025" },
+  { key: "contact", path: "/contact" },
+  { key: "sponsor", path: "/become-a-sponsor" },
+  { key: "visitors", path: "/visitors" },
+  { key: "texpo2025", path: "/texpo-2025" },
 ];
 
 export const STATS: StatItem[] = [
@@ -22,3 +22,4 @@ export const SOCIAL_LINKS = [
   { label: "Facebook", href: "#", icon: "facebook" },
   { label: "Instagram", href: "#", icon: "instagram" },
 ];
+ 
